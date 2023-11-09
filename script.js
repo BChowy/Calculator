ba02cfee6f096b5d23c3b506a8599f51d82c9797
Bushra <75831792+BChowy@users.noreply.github.com>
2023-11-09 12:13:20 +0300

    Declare variables for DOM nodes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const NUMBER_KEYS = document.querySelectorAll('.keypad > .number');
+const OPERATION_KEYS = document.querySelectorAll('.keypad > .operation')
+const DISPLAY_INPUT = document.querySelector('.display-input');
+const PRE_OPERATION = document.querySelector('.display-operation');
+
+const AC_KEY = document.querySelector('#clear');
+const C_KEY = document.querySelector('#backspace');
+const CALCULATE_KEY = document.querySelector('#calculate');
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
